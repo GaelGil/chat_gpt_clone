ce0e6ae3a37a8173167b515ab5e3b893c4c85a7e
gaelgil <gaelgilbello27@gmail.com>
2025-07-01 16:46:41 -0700

    working on simple agent

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI 
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
-from openai import OpenAI
-
+from langchain_core.prompts import ChatMessagePromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
-llm = OpenAI(
-    base_url="http://localhost:8080/v1", 
-    api_key = "sk-no-key-required"
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatOpenAI(
+    openai_api_base="http://localhost:8080/v1",  # Local LLaMA server
+    openai_api_key="sk-no-key-required",         # If your server ignores auth
 )
 
-response = llm.invokte("what is the meaing of life?")
-print(response)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+prompt = ChatPromptTemplate.from_messages([
+    ("system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """
+     ),
+    ('placeholder', '{chat_history}'),
+    ("human", "{input}"),
+    ('placeholder', '{agent_scratchpad}')
+]).partial(format_instructions=parser.get_format_instructions())
+
+
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({'input':'What is the capital of france?'})
+print(raw_response)
+
+structured_response = parser.parse(raw_response.get("output")[0]["text"])
