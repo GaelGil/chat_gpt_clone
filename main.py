4e3f0bac840fccc97503f8c8b7e2c79e59e0bfb3
gaelgil <gaelgilbello27@gmail.com>
2025-07-01 20:15:48 -0700

    testing out agent

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI 
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_core.prompts import ChatMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import PydanticOutputParser
-from langchain.agents import create_tool_calling_agent, AgentExecutor
-
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage,
+)
+from tools import search_tool
 load_dotenv()
 
 class ResearchResponse(BaseModel):
@@ -22,27 +26,37 @@ llm = ChatOpenAI(
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 prompt = ChatPromptTemplate.from_messages([
     ("system",
-            """
-            You are a research assistant that will help generate a research paper.
-            Answer the user query and use neccessary tools. 
-            Wrap the output in this format and provide no other text\n{format_instructions}
-            """
-     ),
-    ('placeholder', '{chat_history}'),
-    ("human", "{input}"),
-    ('placeholder', '{agent_scratchpad}')
+        """
+        You are a research assistant that will help generate a research paper.
+        Answer the user query and use necessary tools. 
+        Wrap the output in this format and provide no other text:\n{format_instructions}
+
+        You can use the following tools:
+        {tools}
+
+        Tool names: {tool_names}
+        """),
+        ("human", "{input}"),
+        ("human", "{agent_scratchpad}"),
 ]).partial(format_instructions=parser.get_format_instructions())
 
 
-
-agent = create_tool_calling_agent(
+tools = [search_tool]
+agent = create_react_agent(
+    tools=tools,
     llm=llm,
-    prompt=prompt,
-    tools=[]
+    prompt=prompt
 )
 
-agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
-raw_response = agent_executor.invoke({'input':'What is the capital of france?'})
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+
+# agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input('What can I help you research? ')
+raw_response = agent_executor.invoke({'input': query, 'agent_scratchpad': '', 'chat_history': []})
 print(raw_response)
 
-structured_response = parser.parse(raw_response.get("output")[0]["text"])
+# try:
+#     structured_response = parser.parse(raw_response.get("output")[0]["text"])
+#     print(structured_response)
+# except Exception as e:
+#     print("Error parsing response", e, "Raw Response - ", raw_response)
