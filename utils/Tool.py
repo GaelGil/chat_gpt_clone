93682efbedf63c5c0cfab21c4d8e71abc24c837b
Gael <gaelgilbello27@gmail.com>
2025-07-04 16:21:21 -0700

    adding helpful files

---
+++ utils/Tool.py
@@ -0,0 +1,5 @@
+class Tool:
+    def __init__(self, name: str, func: function, description: str):
+        self.name = name
+        self.func = func
+        self.description = description
---
+++ utils/config.py
@@ -0,0 +1,7 @@
+BASE_URL:str = 'http://localhost:8080/v1", # "http://<Your api-server IP>:port'
+API_KEY:str = 'sk-no-key-required'
+PROMPT:dict = {
+    'role': 'system',
+    'content': ''
+}
+MODEL_NAME:str = 'LLaMA_CPP'
