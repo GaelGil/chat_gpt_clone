33fbf4d4874cdd1ef958df531a6e298ae388363c
Gael <gaelgilbello27@gmail.com>
2025-07-04 15:26:58 -0700

    added prompt var

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
-BASE_URL = 'http://localhost:8080/v1", # "http://<Your api-server IP>:port'
-API_KEY = 'sk-no-key-required'
+BASE_URL:str = 'http://localhost:8080/v1", # "http://<Your api-server IP>:port'
+API_KEY:str = 'sk-no-key-required'
+PROMPT:dict = {
+    'role': 'system',
+    'content': ''
+}
