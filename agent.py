066933028996a5aa7c374d283027f9dca9086d15
Gael <gaelgilbello27@gmail.com>
2025-07-04 15:27:11 -0700

    upating agent

--- agent.py
+++ agent.py
@@ -2,4 +2,5 @@ from .llm.ModelClient import ModelClient
 from .llm.Model import Model
 from config import BASE_URL, API_KEY
 
-client: ModelClient = ModelClient(BASE_URL, API_KEY)
+client: ModelClient = ModelClient(url=BASE_URL, key=API_KEY)
+llm: Model = Model(client=client)
