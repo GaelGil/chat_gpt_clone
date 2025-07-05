c2b228f91769c1cc6a918071e959ea2409235147
gaelgil <gaelgilbello27@gmail.com>
2025-07-04 19:29:13 -0700

    updating project

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-from .llm.ModelClient import ModelClient
-from .llm.Model import Model
+from llm.ModelClient import ModelClient
+from llm.Model import Model
+from openai import OpenAI
 from utils.config import CONFIG
 from Agent import Agent
 
 if __name__ == "__main__":
-    client: ModelClient = ModelClient(url=CONFIG['BASE_URL'], key=CONFIG['API_KEY'])
-    llm: Model = Model(client=client, model_name=CONFIG['MODEL_NAME'])
-
+    client: OpenAI = ModelClient(url=CONFIG['BASE_URL'], key=CONFIG['API_KEY']).get_client()
+    
     tools = []
-    output_format = ''
-    function_call = {'call': 'tool.name'}
     CONFIG['PROMPT'] = f"""
-    You are an AI assistant tasked with {CONFIG['GOAL']} with these {tools} to help. You must help the user with
-    whatever question/task they need. You must resturn the your output in this forrmat {output_format}.
-    If you need to call a function simply return {function_call}
+    You are an AI assistant tasked with {CONFIG['GOAL']}. You have tools available here {tools}. You must help the user with
+    whatever question/task they need. You must return ONLY in this forrmat {CONFIG['OUTPUT_FORMAT']}.
     """
 
-    agent = Agent(model=llm, tools=tools, goal=CONFIG['GOAL'])
-    agent.model.set_prompt(prompt=CONFIG['PROMPT'])
+    llm: Model = Model(client=client,
+                        system_prompt= {"role": "system",
+                                        "content": CONFIG['SYSTEM_PROMPT']
+                                        },
+                        tools=[],
+                        model_name=CONFIG['MODEL_NAME'])
+
+
+
+    agent: Agent = Agent(model=llm)
     agent.start()
 
--- utils/Tool.py
+++ utils/Tool.py
@@ -1,5 +1,6 @@
 class Tool:
-    def __init__(self, name: str, func: function, description: str):
+    def __init__(self, name: str, func: function, param: dict, description: str):
         self.name = name
         self.func = func
+        self.param = param
         self.description = description
