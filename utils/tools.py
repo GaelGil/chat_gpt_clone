e0186b8d22d0148eb08bdf988f0173c410295708
gaelgil <gaelgilbello27@gmail.com>
2025-07-08 21:57:41 -0700

    added registry

--- utils/tools.py
+++ utils/tools.py
@@ -1,29 +1,23 @@
 from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
 from langchain_community.utilities import WikipediaAPIWrapper
-from .Tool import Tool
 from datetime import datetime
+from typing import Callable, Any
+
+TOOL_REGISTRY: dict[str, Callable[..., Any]] = {}
+# ... any nummber and type of args
+# Any any return type
+def register_tool(name: str) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
+        TOOL_REGISTRY[name] = func
+        return func
+    return decorator
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def save_to_txt(data: str, filename: str = "research_output.txt"):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
+@register_tool('save_text_to_file')
+def save_to_txt(data: str, filename: str = "./research_output.txt") -> str:
+    timestamp: datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_text: str = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
 
     with open(filename, "a", encoding="utf-8") as f:
         f.write(formatted_text)
@@ -32,23 +26,16 @@ def save_to_txt(data: str, filename: str = "research_output.txt"):
 
 
 search = DuckDuckGoSearchRun()
-search_tool = Tool(
-    name="searchhhhhhhhh",
-    func=search.run,
-    param=None,
-    description="Search the web for information",
-)
-save_tool: Tool = Tool(
-    name="save_text_to_filehhhhh",
-    func=save_to_txt,
-    param='filename',
-    description="Saves structured research data to a text file.",
-)
+
+@register_tool('search_web')
+def search_web(query: str) -> str:
+    return search.run(query)
 
 
 
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
-wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+wiki_query_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
 
-
-TOOLS = [save_tool.to_dict(), search_tool.to_dict()]
+@register_tool('wiki_query')
+def wiki_query(query: str) -> str:
+    return wiki_query_tool.run(query=query)
