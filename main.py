d0503dd0e38332df17da1ac4e7a8ae744f74657e
Gael <gaelgilbello27@gmail.com>
2025-07-09 11:06:14 -0700

    removed white space

--- main.py
+++ main.py
@@ -7,19 +7,18 @@ from utils.tools import TOOL_REGISTRY, TOOL_DEFINITIONS
 
 if __name__ == "__main__":
     client: OpenAI = ModelClient(url=CONFIG['BASE_URL'], key=CONFIG['API_KEY']).get_client()
-    
-    CONFIG['SYSTEM_PROMPT'] = f"""
+    CONFIG['PROMPT'] = f"""
     You are an AI assistant tasked with {CONFIG['GOAL']}. You have tools available here {TOOL_DEFINITIONS}. You must help the user with
     whatever question/task they need. You must return ONLY in this forrmat {CONFIG['OUTPUT_FORMAT']}.
     """
 
     llm: Model = Model(client=client,
                         system_prompt= {"role": "assistant",
-                                        "content": CONFIG['SYSTEM_PROMPT']
+                                        "content": CONFIG['PROMPT']
                                         },
                         tools=TOOL_DEFINITIONS,
                         model_name=CONFIG['MODEL_NAME'])
-    print(f'TOOL_REGISTRY: {TOOL_REGISTRY}')
+    
     agent: Agent = Agent(model=llm, functions=TOOL_REGISTRY)
     agent.start()
 
