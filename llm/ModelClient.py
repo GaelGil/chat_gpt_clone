78a65901de49dc97ae4a8a23cbe2df8819d95fb1
Gael <gaelgilbello27@gmail.com>
2025-07-09 22:53:04 -0700

    updated comments

--- llm/ModelClient.py
+++ llm/ModelClient.py
@@ -6,10 +6,18 @@ class ModelClient:
 
     def __init__(self, key: str) -> None:
         """
+        Args:
+            None
+        Returns:
+            None
         """
         self.client = OpenAI(api_key=key)
 
     def get_client(self) -> OpenAI:
-        """
+        """Return the openai client
+        Args: 
+            None
+        Returns:
+            OpenAI
         """
         return self.client
