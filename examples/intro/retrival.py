d12f548d65c403bbe7a2d2ed53813e7d3acb5889
Gael <gaelgilbello27@gmail.com>
2025-07-10 17:22:02 -0700

    updated examples structure

---
+++ examples/intro/retrival.py
@@ -0,0 +1,100 @@
+import json
+import os
+from openai import OpenAI
+from pydantic import BaseModel, Field
+
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+def search_kb(question: str):
+    """
+    Load the whole knowledge base from the JSON file.
+    (This is a mock function for demonstration purposes, we don't search)
+    """
+    with open("kb.json", "r") as f:
+        return json.load(f)
+    
+tools = [
+    {
+        'type': 'function',
+        'name': 'search_kb',
+        'description': 'Get the answer to the users question from the knowledge base',
+        'parameters': {
+            'type': 'object',
+            'properties': {
+                'question': {'type': 'string'}
+            },
+            'required': ['question'],
+            'additionalProperties': False,
+        },
+        'strict': True       
+    }
+]
+
+system_prompt = 'You are a helpful assistant that answers questions from the knowledge base about our e-commerce store'
+
+messages = [
+    {
+        'role': 'developer',
+        'content': system_prompt
+    },
+    {
+        'rolse': 'user',
+        'content': 'what is the return policy?'
+    }
+]
+
+completion = client.responses.create(
+    model='gpt-4.1-mini',
+    input=messages,
+    tools=tools
+)
+
+
+completion.model_dump()
+
+def call_function(name, args):
+    if name == 'search_kb':
+        return search_kb(**args)
+    
+for tool_call in completion.choices[0].message.tool_calls:
+    name = tool_call.function.name
+    args = json.loads(tool_call.function.arguments)
+    messages.append(completion.choices[0].message)
+
+    result = call_function(name, args)
+    messages.append(
+        {
+            'role': 'tool',
+            'tool_call_id': tool_call.id,
+            'content': json.dumps(result)
+        }
+    )
+
+
+class KBResponse(BaseModel):
+    answer: str = Field(description='The answer to the users question')
+    source: int = Field(description='The record id of the answer')
+
+completion_two = client.beta.chat.completions.parse(
+    model='gpt-4.1-mini',
+    messages=messages,
+    tools=tools,
+    response_format=KBResponse
+)
+
+final_response = completion_two.choices[0].message.parsed
+final_response.answer
+final_response.source
+
+messages = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": "What is the weather in Tokyo?"},
+]
+
+completion_3 = client.beta.chat.completions.parse(
+    model="gpt-4o",
+    messages=messages,
+    tools=tools,
+)
+
+completion_3.choices[0].message.content
