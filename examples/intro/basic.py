eb86dedb0f6b49c4312f335a82cf04127cb30a00
Gael <gaelgilbello27@gmail.com>
2025-07-15 13:37:29 -0700

    conncected llm calls to client

--- examples/intro/basic.py
+++ examples/intro/basic.py
@@ -1,4 +1,4 @@
-from Model import LLM
+from examples.Model import LLM
 
 model = LLM(model_name="gpt-4.1-mini")
 
--- examples/intro/retrival.py
+++ examples/intro/retrival.py
@@ -1,74 +1,78 @@
 import json
-import os
-from openai import OpenAI
 from examples.models.schemas import KBResponse
-from dotenv import load_dotenv
-from pathlib import Path
 from examples.Model import LLM
-
-load_dotenv(Path("../.env"))
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-
-llm = LLM(model_name="gpt-4.1-mini")
-
-# the system prompt (developer)
-system_prompt = "You are a helpful assistant that answers questions from the knowledge base about our e-commerce store"
-
-# the inpiut messages
-messages = [
-    {"role": "developer", "content": system_prompt},
-    {"rolse": "user", "content": "what is the return policy?"},
-]
-
-# create response
-response = llm.create_response(messages=messages, tools=tools)
+from mcp.client import MCPClient
+import asyncio
 
 
-# the function that the model can call
-def call_function(name, args):
-    if name == "search_kb":
-        return search_kb(**args)
+def basic_retrival(llm: LLM, tools: list, mcp_client: MCPClient):
+    # the system prompt (developer)
+    system_prompt = "You are a helpful assistant that answers questions from the knowledge base about our e-commerce store"
 
+    # the inpiut messages
+    messages = [
+        {"role": "developer", "content": system_prompt},
+        {"rolse": "user", "content": "what is the return policy?"},
+    ]
 
-# for every tool call in the response
-for tool_call in response.output:
-    if tool_call.type != "function_call":
-        continue
-    # select tool name
-    name = tool_call.name
-    # get the tool arguments
-    args = json.loads(tool_call.arguments)
-    # add the previous response output to input messages (tool_call = item in response.output)
-    messages.append(tool_call)
-    # call our function
-    result = call_function(name, args)
-    # add the result of that to our input messages
-    messages.append(
-        {
-            "type": "function_call_output",
-            "call_id": tool_call.call_id,
-            "output": str(result),
-        }
+    # create response
+    response = llm.create_response(messages=messages, tools=tools)
+
+    # the function that the model can call
+    async def call_function(name, args):
+        if name == "search_kb":
+            res = await mcp_client.call_tool(name, args)
+            return res
+
+    # for every tool call in the response
+    for tool_call in response.output:
+        if tool_call.type != "function_call":
+            continue
+        # select tool name
+        name = tool_call.name
+        # get the tool arguments
+        args = json.loads(tool_call.arguments)
+        # add the previous response output to input messages (tool_call = item in response.output)
+        messages.append(tool_call)
+        # call our function
+        result = call_function(name, args)
+        # add the result of that to our input messages
+        messages.append(
+            {
+                "type": "function_call_output",
+                "call_id": tool_call.call_id,
+                "output": str(result),
+            }
+        )
+
+    # get a response and add our desired response format
+    response_two = llm.parse_response(
+        messages=messages, tools=tools, response_format=KBResponse
     )
 
+    # print the final response
+    final_response = response_two.output_parsed
+    print(final_response)
 
-# get a response and add our desired response format
-response_two = llm.parse_response(
-    messages=messages, tools=tools, response_format=KBResponse
-)
+    # this wont cause a function call because the model does not have information/tool on how to answer
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": "What is the weather in Tokyo?"},
+    ]
 
-# print the final response
-final_response = response_two.output_parsed
-print(final_response)
+    response_three = llm.parse_response(messages=messages, tools=tools)
+
+    print(response_three)
 
 
-# this wont cause a function call because the model does not have information/tool on how to answer
-messages = [
-    {"role": "system", "content": system_prompt},
-    {"role": "user", "content": "What is the weather in Tokyo?"},
-]
+async def main():
+    client = MCPClient()
+    await client.connect()
+    tools = await client.list_tools()
+    llm = LLM(model_name="gpt-4.1-mini")
 
-response_three = llm.parse_response(messages=messages, tools=tools)
+    basic_retrival(llm=llm, tools=tools)
 
-print(response_three)
+
+if __name__ == "__main__":
+    asyncio.run(main())
