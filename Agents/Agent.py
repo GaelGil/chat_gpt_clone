a88ab4215385f28fa3855d8792a79d4b627bd0d2
Gael <gaelgilbello27@gmail.com>
2025-07-23 13:49:05 -0700

    updating project strucuure

--- Agents/Agent.py
+++
@@ -1,9 +0,0 @@
-class ResponseAgent:
-    def __init__(self, messages: list = []):
-        self.messages = []
-
-    def add_message(self, role: str, content: str):
-        self.messages.append({"role": role, "content": content})
-
-    def generate_response(self):
-        pass
