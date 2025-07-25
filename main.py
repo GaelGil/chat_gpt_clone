28bfdf19b9db7679025c7837246c95a53b3cdb95
Gael <gaelgilbello27@gmail.com>
2025-07-25 11:30:37 -0700

    updates

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
 from LLM.LLM import LLM
 from dotenv import load_dotenv
+from pathlib import Path
 from Agents.PlannerAgent import PlannerAgent
 from Agents.OrchestratorAgent import OrchestatorAgent
+import prompts
 from MCP.client import MCPClient
 
-load_dotenv()
+load_dotenv(Path("./.env"))
+
+mcp_client = MCPClient()
+print(mcp_client.list_tools())
+# TODO: connect to client
 
 llm_model = LLM(api_key=os.getenv("OPENAI_API_KEY"))
 
-client = MCPClient()
-
 planner = PlannerAgent(
-    dev_prompt="",
-    mcp_client=None,
+    dev_prompt=prompts.PLANNER_AGENT_PROMPT,
+    mcp_client=mcp_client,
     llm=llm_model,
     messages=[],
     max_turns=3,
@@ -21,8 +25,8 @@ planner = PlannerAgent(
 )
 
 orchestrator = OrchestatorAgent(
-    dev_prompt="",
-    mcp_client=None,
+    dev_prompt=prompts.ORCHESTRATOR_AGENT_PROMPT,
+    mcp_client=mcp_client,
     llm=llm_model,
     messages=[],
     max_turns=3,
