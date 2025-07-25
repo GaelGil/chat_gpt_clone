bb2658fbc9f31ce81237fbd970c026df2ffc7b84
Gael <gaelgilbello27@gmail.com>
2025-07-24 17:19:08 -0700

    updating agent attrivutes

---
+++ Agents/OrchestratorAgent.py
@@ -0,0 +1,8 @@
+class OrchestatorAgent:
+    def __init__(self, dev_prompt, mcp_client, llm, messages, max_turns, tools):
+        self.dev_prompt = dev_prompt
+        self.mcp_client = mcp_client
+        self.llm = llm
+        self.max_turns = max_turns
+        self.messages = messages
+        self.tools = tools
