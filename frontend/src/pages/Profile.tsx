16fd1e5355f50ba35ab1ab898408c1912a4fe93a
Gael <gaelgilbello27@gmail.com>
2025-09-11 14:24:15 -0700

    updating frontend

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,8 +1,13 @@
 import UserProfile from "../components/User/UserProfile";
-
+import Navigation from "../components/Layout/NavBar";
 // home component
 const ProfilePage: React.FC = () => {
-  return <UserProfile />;
+  return (
+    <>
+      <Navigation />
+      <UserProfile />
+    </>
+  );
 };
 
 export default ProfilePage;
