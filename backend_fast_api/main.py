5da6b1a6153af700b06dfa442c20a2c2ca7ec5b4
Gael <gaelgilbello27@gmail.com>
2025-09-26 23:55:58 -0700

    added more files to template

--- backend_fast_api/main.py
+++ backend_fast_api/main.py
@@ -1,21 +1,32 @@
+import sentry_sdk
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.extensions import init_db
-from app.user.controller import router as user_router
+from fastapi.routing import APIRoute
+from starlette.middleware.cors import CORSMiddleware
+from app.api.main import api_router
+from app.core.config import settings
 
-app = FastAPI()
 
-# CORS for React frontend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
+    sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    generate_unique_id_function=custom_generate_unique_id,
 )
 
-# DB setup
-init_db(app)
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-# Routers
-app.include_router(user_router, prefix="/api/users", tags=["users"])
+app.include_router(api_router, prefix=settings.API_V1_STR)
