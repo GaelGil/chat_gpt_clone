12377e21aa1e9b6f816951f640aa91b6beb73f11
Gael <gaelgilbello27@gmail.com>
2025-09-27 16:50:04 -0700

    updated the common

---
+++ backend_fast_api/app/models/Common.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datetime, timezone
+from decimal import Decimal
+
+from sqlmodel import Field, SQLModel
+
+
+# Generic message
+class Message(SQLModel):
+    message: str
+
+
+# JSON payload containing access token
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+# Contents of JWT token
+class TokenPayload(SQLModel):
+    sub: str | None = None
+
+
+class NewPassword(SQLModel):
+    token: str
+    new_password: str = Field(min_length=8, max_length=40)
