d71b134cda633b04af8216f9af5eee98c41c430a
Gael Gil <gaelgilbello27@gmail.com>
2025-12-10 15:02:38 -0800

    removed unused stuff

--- frontend/src/components/Chat/ChatSideBar.tsx
+++ frontend/src/components/Chat/ChatSideBar.tsx
@@ -4,47 +4,15 @@ import { ActionIcon } from "@mantine/core";
 import { FiArrowRight, FiColumns, FiEdit } from "react-icons/fi";
 import { PROJECT_NAME, LOGO } from "@/const";
 import { useState } from "react";
-import { SessionService } from "@/client";
-import { useQuery } from "@tanstack/react-query";
+
 import Chats from "./Chats";
 interface SidebarProps {
   collapsed: boolean;
   toggle: () => void;
 }
 
-// function getUsersQueryOptions({ page }: { page: number }) {
-//   return {
-//     queryFn: () => SessionService.getSessions(),
-//     queryKey: ["sessions"],
-//   };
-// }
 const ChatSideBar: React.FC<SidebarProps> = ({ collapsed, toggle }) => {
-  // const sessions;
-  // const { data, isLoading, isPlaceholderData } = useQuery({
-  //   ...getUsersQueryOptions({ page }),
-  //   placeholderData: (prevData) => prevData,
-  // });
-
-  // const items = data?.data ?? [];
   const [hovered, setHovered] = useState(false);
-
-  // const listItems = items.map((item) => (
-  //   <>
-  //     {collapsed ? (
-  //       <></>
-  //     ) : (
-  //       <Flex mb="sm">
-  //         <Link
-  //           to="/chat/$chatId"
-  //           params={{ chatId: item.id.toString() }}
-  //           key={item.id}
-  //         >
-  //           <Text fz={"sm"}>{item.title}</Text>
-  //         </Link>
-  //       </Flex>
-  //     )}
-  //   </>
-  // ));
 
   return (
     <Stack>
