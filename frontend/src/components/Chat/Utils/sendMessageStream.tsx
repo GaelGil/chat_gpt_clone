0152069f54e991394b2a4c86ba45cb56d85e3c98
Gael Gil <gaelgilbello27@gmail.com>
2025-12-29 15:55:15 -0800

    updating call function

--- frontend/src/components/Chat/Utils/sendMessageStream.tsx
+++ frontend/src/components/Chat/Utils/sendMessageStream.tsx
@@ -1,16 +1,13 @@
 import { NewMessage } from "@/client";
-// import autht
 export async function sendMessageStream(
   sessionId: string,
   message: NewMessage,
   signal?: AbortSignal
 ): Promise<ReadableStream<Uint8Array>> {
-  console.log("Sending token:", localStorage.getItem("token"));
   const res = await fetch(`/api/v1/session/${sessionId}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
     credentials: "include",
     body: JSON.stringify(message),
