3ad72dbc52442a14475b85a018bf2f2e53405d62
Gael Gil <gaelgilbello27@gmail.com>
2026-01-08 15:54:53 -0800

    working on providers

---
+++ backend/app/providers/BaseProvider.py
@@ -0,0 +1,95 @@
+import asyncio
+import uuid
+from typing import Protocol
+
+from fastapi import HTTPException
+
+from app.models import Message, ToolCall
+from app.schemas.Message import NewMessage, Role, Status
+
+
+class BaseProvider(Protocol):
+    def save_message(
+        self, session_id: uuid.UUID, owner_id: uuid.UUID, new_message: NewMessage
+    ) -> tuple[uuid.UUID | None, HTTPException | None]:
+        """Save user message to session
+
+        Args:
+            session_id (uuid.UUID): session id
+            owner_id (uuid.UUID): user id
+            new_message (NewMessage): message
+
+        Returns:
+            tuple[uuid.UUID| None, HTTPException | None]:"""
+
+        message_obj = Message.model_validate(
+            new_message, update={"owner_id": owner_id, "session_id": session_id}
+        )
+        try:
+            self.session.add(message_obj)
+            self.session.commit()
+        except Exception as e:
+            self.session.rollback()
+            return None, HTTPException(status_code=400, detail=str(e))
+
+        return message_obj.id, None
+
+    async def update_message_async(
+        self, message_id: uuid.UUID, status: Status, role: Role, content: str
+    ):
+        await asyncio.to_thread(self.update_message, message_id, status, role, content)
+
+    def update_message(
+        self, message_id: uuid.UUID, status: Status, role: Role, content: str
+    ) -> tuple[uuid.UUID | None, HTTPException | None]:
+        """
+        Args:
+            message_id (uuid.UUID): descriptiond
+            status (Status): description
+            role (Role): description
+            content (str): description
+
+        """
+        msg = self.session.get(Message, message_id)
+        msg.status = status
+        msg.role = role
+        msg.content = content
+        try:
+            self.session.commit()
+        except Exception as e:
+            self.session.rollback()
+            return None, HTTPException(status_code=400, detail=str(e))
+
+        return msg.id, None
+
+    async def save_tool_call_async(
+        self,
+        session_id: uuid.UUID,
+        name: str,
+        args: dict,
+        result: str,
+        owner_id: uuid.UUID,
+    ):
+        await asyncio.to_thread(
+            self.save_tool_call, session_id, name, args, result, owner_id
+        )
+
+    def save_tool_call(
+        self,
+        session_id: uuid.UUID,
+        name: str,
+        args: dict,
+        result: str,
+        owner_id: uuid.UUID,
+    ):
+        tool_call_obj = ToolCall(
+            name=name,
+            args=args,
+            result=result,
+            owner_id=owner_id,
+            session_id=session_id,
+        )
+
+        self.session.add(tool_call_obj)
+        self.session.commit()
+        pass
