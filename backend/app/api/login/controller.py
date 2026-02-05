a125627893a17869612a752df50366269c5e60b0
Gael Gil <gaelgilbello27@gmail.com>
2026-02-05 11:50:16 -0800

    fixed more imports

--- backend/app/api/login/controller.py
+++ backend/app/api/login/controller.py
@@ -10,9 +10,9 @@ from app.api.deps import CurrentUser, SessionDep, get_current_active_superuser
 from app.core import security
 from app.core.config import settings
 from app.core.security import get_password_hash
-from app.models import Message
-from app.schemas.User import UserPublic
-from app.schemas.Utils import NewPassword, Token
+from app.database.models import Message
+from app.database.schemas.User import UserPublic
+from app.database.schemas.Utils import NewPassword, Token
 from app.utils import (
     generate_password_reset_token,
     generate_reset_password_email,
--- backend/app/api/private/controller.py
+++ backend/app/api/private/controller.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel
 
 from app.api.deps import SessionDep
 from app.core.security import get_password_hash
-from app.models import User
-from app.schemas.User import UserPublic
+from app.database.models import User
+from app.database.schemas.User import UserPublic
 
 router = APIRouter(tags=["private"], prefix="/private")
 
